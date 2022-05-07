c3a9da7aa1ad56b596ce27c817d3900b7d7a05cd
anahitaneogi <96186305+anahitaneogi@users.noreply.github.com>
2022-05-07 12:43:09 +0200

    Practice programs for learning string manipulation

---
+++ Strings.py
@@ -0,0 +1,18 @@
+#Make a program that displays your favourite actor/actress.
+#Try to print the word ‘lucky’ inside s.
+#Try to print the day, month, year in the form “Today is 2/2/2016”.
+
+Housebud1 = "Anahita"
+Housebud2 = "Arya"
+combination = Housebud1[0:2] + Housebud2[0:4]
+print(combination)
+
+classmateIND = "Anahita"
+classmateITAl = "Soann"
+combination = classmateIND[0:3] + classmateITAl[0:3]
+print(combination)
+
+FLEbud1 = "Alketa"
+FLEbud2 = "Anahita"
+combination = FLEbud1[0:3] + FLEbud2[0:2]
+print(combination)
